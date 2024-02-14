0b46cb8af5ea2e2d22267ff00ae8dfb55fd02e2c
vivek-investwell <vivek.gaur@investwellonline.com>
2024-02-14 18:08:06 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginIndex from './uicollection/login/Index';
 import SignUpIndex from './uicollection/signup/Index';
 import HomeIndex from './uicollection/home/Index';
+import PolicyDocs from './uicollection/PolicyDocs';
 function App() {
   return (
    <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element = {<LoginIndex/>}/>
       <Route path='/signUp' element = {<SignUpIndex/>}/>
       <Route path='/home' element={<HomeIndex/>}></Route>
+      <Route path='/policy' element={<PolicyDocs/>}></Route>
     </Routes>
    </BrowserRouter>
   );
